febf10568480f2b33f1c8b384dfee9891fcd3fb0
sdokus <sami2464@gmail.com>
2022-03-24 16:05:52 -0400

    added pushing players into a room with a game

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -1,34 +1,40 @@
 import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import socket from "./socket";
 import { Link } from "react-router-dom";
+import { fetchAllCrossword } from "../store/crossword";
 
 export default function Lobby(props) {
+  const crosswords = useSelector((state) => state.dataReducer.allCrossword);
+  const dispatch = useDispatch();
+  window.localStorage.setItem("puzzle", "{}");
+
   const [state, setState] = useState({
     players: [],
     host: "",
-    // selectedPuzzle: "" (eventually this could be where we store which puzzle the players select? )
+    selectedPuzzle: "2017-01-04",
   });
 
   useEffect(() => {
+    dispatch(fetchAllCrossword());
+
     const room = window.localStorage.getItem("roomId"); //the roomId is passed to the lobby through localStorage
     socket.emit("get-host", room);
 
     loadUsers();
     socket.on("render-users", (playerInfo) => {
-      setState({ players: playerInfo, host: playerInfo[0].nickname });
+      setState({ ...state, players: playerInfo, host: playerInfo[0].nickname });
     });
-
-    // socket.on("set-host", (host) => {
-    //   setState({ ...state, host: host });
-    //   console.log(state.host);
-    // });
 
     socket.on("update-users", () => {
       loadUsers();
     });
 
-    socket.on("begin-session", (mode) => {
-      props.history.push(`/${mode}/${room}`);
+    socket.on("begin-session", (puzzle) => {
+      const roomId = window.localStorage.getItem("roomId");
+      window.localStorage.setItem("puzzle", JSON.stringify(puzzle));
+      props.history.push(`/game/${roomId}`);
+      // props.history.push(`/${puzzle}/${room}`);
     });
   }, []);
 
@@ -37,31 +43,36 @@ export default function Lobby(props) {
     socket.emit("load-users", roomId);
   }
 
+  //function to change which puzzle is currently selected:
+  function selectPuzzle(puzzle) {
+    window.localStorage.setItem("puzzle", JSON.stringify(puzzle));
+    setState({ ...state, selectedPuzzle: puzzle.date });
+  }
+
+  //handler function to push all players into a game room with the selected puzzle
+  function startSession(puzzle) {
+    const roomId = window.localStorage.getItem("roomId");
+    selectPuzzle(puzzle);
+    socket.emit("start-session", roomId, puzzle);
+    props.history.push(`/game/${roomId}`);
+    // props.history.push(`/${state.selectedPuzzle}/${roomId}`);
+  }
+
+  //handler function to add the URL with room id to the clipboard
   function handleClick() {
     const roomId = window.localStorage.getItem("roomId");
     navigator.clipboard.writeText(`${window.location.host}/?` + roomId);
     // alert("Link copied to clipboard!");
   }
+
+  const isUserHost = window.localStorage.getItem("host");
+  const room = window.localStorage.getItem("roomId");
+
   return (
     <div className="splash-container">
       <div className="splash">
-        <h2
-        // style={{
-        //   textAlign: "center",
-        //   marginTop: "9px",
-        // }}
-        >{`${state.host}'s Lobby`}</h2>
-        <div
-        // style={{
-        //   flexWrap: "nowrap",
-        //   width: "430px",
-        //   justifyContent: "space-between",
-        //   alignItems: "center",
-        //   alignContent: "center",
-        //   textAlign: "center",
-        //   height: "260px",
-        // }}
-        >
+        <h2>{`${state.host}'s Lobby`}</h2>
+        <div>
           {state.players.map((player, i) => {
             return (
               <div md={4} key={i}>
@@ -72,9 +83,52 @@ export default function Lobby(props) {
             );
           })}
         </div>
-        <Link to="/game">
-          <button className="button-success pure-button">Start Game</button>
-        </Link>
+
+        {crosswords.map((puzzle, ind) => {
+          return (
+            <div
+              className="card"
+              key={ind}
+              style={{
+                width: "18rem",
+                height: "350px",
+                alignItems: "center",
+                justifyContent: "center",
+                margin: "5px",
+              }}
+            >
+              <div style={{ fontFamily: "Fuzzy Bubbles, cursive" }}>
+                {puzzle.difficulty}
+              </div>
+              <div style={{ textAlign: "center" }}>{puzzle.name}</div>
+              {isUserHost === "true" ? (
+                // <Link to={`/${puzzle.date}/${room}`}>
+                <Link to={`/game/${room}`}>
+                  <button
+                    type="submit"
+                    value={puzzle.date}
+                    onClick={() => {
+                      startSession(puzzle);
+                    }}
+                  >
+                    Start Puzzle
+                  </button>
+                </Link>
+              ) : (
+                <br />
+              )}
+            </div>
+          );
+        })}
+        {/* <Link to="/game">
+          <button
+            className="button-success pure-button"
+            type="button"
+            onClick={() => startSession()}
+          >
+            Start Game
+          </button>
+        </Link> */}
         <div style={{ marginTop: "130px" }}>
           <button
             className="button-success pure-button"
--- client/components/SingleCrossword.js
+++ client/components/SingleCrossword.js
@@ -1,15 +1,15 @@
-import React, { useRef, useCallback, useState, useEffect } from "react"
-import { crossBoard1 } from "./crossWord"
-import { getGuess, fetchAllCrossword } from "../store/crossword"
-import store from "../store"
+import React, { useRef, useCallback, useState, useEffect } from "react";
+import { crossBoard1 } from "./crossWord";
+import { getGuess, fetchAllCrossword } from "../store/crossword";
+import store from "../store";
 
 import Crossword, {
   CrosswordImperative,
   CrosswordProvider,
-} from "@jaredreisinger/react-crossword"
-import io from "socket.io-client"
-import { useSelector, useDispatch } from "react-redux"
-import socket from "./socket"
+} from "@jaredreisinger/react-crossword";
+import io from "socket.io-client";
+import { useSelector, useDispatch } from "react-redux";
+import socket from "./socket";
 //import { RootState } from "../store"
 
 // console.log(crossBoard1)
@@ -25,32 +25,31 @@ import socket from "./socket"
 // })
 
 export default function MyPage() {
-
-  const dispatch = useDispatch()
-  const crosswords = useSelector((state) => state.dataReducer.allCrossword)
+  const dispatch = useDispatch();
+  const crosswords = useSelector((state) => state.dataReducer.allCrossword);
 
   //const crossword = useRef < CrosswordImperative > null
-  const crossword = useRef(null)
+  const crossword = useRef(null);
 
   useEffect(() => {
-    dispatch(fetchAllCrossword())
-    
+    dispatch(fetchAllCrossword());
+
     // socket.on('crosswar', (payload) => {
     //   store.dispatch(getGuess(payload.row, payload.col, payload.char));
     // });
 
-    window.localStorage.setItem('correctClues', '[]');
-    window.localStorage.setItem('correctCells', '[]');
+    window.localStorage.setItem("correctClues", "[]");
+    window.localStorage.setItem("correctCells", "[]");
 
-    socket.on('newWord', (payload) => {
-      const corrects = JSON.parse(window.localStorage.getItem('correctClues'));
+    socket.on("newWord", (payload) => {
+      const corrects = JSON.parse(window.localStorage.getItem("correctClues"));
       const newCorrect = `${payload.number} ${payload.direction}`;
-      const cells = JSON.parse(window.localStorage.getItem('correctCells'));
+      const cells = JSON.parse(window.localStorage.getItem("correctCells"));
       if (!corrects.includes(newCorrect)) {
         corrects.push(newCorrect);
-        window.localStorage.setItem('correctClues', JSON.stringify(corrects));
+        window.localStorage.setItem("correctClues", JSON.stringify(corrects));
       }
-      if (payload.direction === 'across') {
+      if (payload.direction === "across") {
         const start = [
           crossBoard1.across[payload.number].row,
           crossBoard1.across[payload.number].col,
@@ -71,26 +70,26 @@ export default function MyPage() {
           start[0]++;
         }
       }
-      window.localStorage.setItem('correctCells', JSON.stringify(cells));
+      window.localStorage.setItem("correctCells", JSON.stringify(cells));
     });
   }, []);
 
   const onCellChange = (row, col, char) => {
     console.log(row, col, char);
-    const cells = JSON.parse(window.localStorage.getItem('correctCells'));
+    const cells = JSON.parse(window.localStorage.getItem("correctCells"));
     if (
       cells.some(
-        (cell) => cell.split(', ').slice(0, 2).join(', ') === `${row}, ${col}`
+        (cell) => cell.split(", ").slice(0, 2).join(", ") === `${row}, ${col}`
       )
     ) {
-      console.log('ALREADY CORRECT');
+      console.log("ALREADY CORRECT");
       const correctLetter = cells
         .find(
-          (cell) => cell.split(', ').slice(0, 2).join(', ') === `${row}, ${col}`
+          (cell) => cell.split(", ").slice(0, 2).join(", ") === `${row}, ${col}`
         )
-        .split(', ')
+        .split(", ")
         .slice(2)
-        .join('');
+        .join("");
       console.log(correctLetter);
       if (char !== correctLetter) {
         setTimeout(() => {
@@ -101,30 +100,30 @@ export default function MyPage() {
   };
 
   const onCorrect = (direction, number, answer) => {
-    const corrects = JSON.parse(window.localStorage.getItem('correctClues'));
+    const corrects = JSON.parse(window.localStorage.getItem("correctClues"));
     const newCorrect = `${number} ${direction}`;
     if (!corrects.includes(newCorrect)) {
-      console.log('CORRECT');
-      socket.emit('correctWord', { direction, number, answer });
+      console.log("CORRECT");
+      socket.emit("correctWord", { direction, number, answer });
       corrects.push(newCorrect);
     }
-    window.localStorage.setItem('correctClues', JSON.stringify(corrects));
+    window.localStorage.setItem("correctClues", JSON.stringify(corrects));
     // for (let i = 0; i < answer.length; i++) {
     //   crossword.current?.setGuess(start[0], start[1], answer[i])
     //   start[1]++;
     // }
   };
-
+  const selectedPuzzle = JSON.parse(window.localStorage.getItem("puzzle"));
   return (
     <div style={{ height: 200, width: 400 }}>
       <Crossword
         onCorrect={onCorrect}
         onCellChange={onCellChange}
         ref={crossword}
-        data={crossBoard1}
+        data={JSON.parse(selectedPuzzle.data)}
 
         // useStorage={false}
       />
     </div>
-  )
+  );
 }
