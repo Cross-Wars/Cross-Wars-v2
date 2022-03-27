456b4c7557b2c138aba4c693dd59bf651bd382b4
Lucas McGill <lmlucasmc8@gmail.com>
2022-03-27 12:52:11 -0400

    added logo for each puzzle

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -1,73 +1,73 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import socket from "./socket";
-import { Link } from "react-router-dom";
-import { fetchAllCrossword } from "../store/crossword";
-import { Button } from "@material-ui/core";
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import socket from './socket';
+import { Link } from 'react-router-dom';
+import { fetchAllCrossword } from '../store/crossword';
+import { Button } from '@material-ui/core';
 
 export default function Lobby(props) {
   const crosswords = useSelector((state) => state.dataReducer.allCrossword);
   const dispatch = useDispatch();
-  window.localStorage.setItem("puzzle", "{}");
+  window.localStorage.setItem('puzzle', '{}');
 
   const [state, setState] = useState({
     players: [],
-    host: "",
-    selectedPuzzle: "2017-01-04",
+    host: '',
+    selectedPuzzle: '2017-01-04',
   });
 
   useEffect(() => {
     dispatch(fetchAllCrossword());
 
-    const room = window.localStorage.getItem("roomId"); //the roomId is passed to the lobby through localStorage
-    socket.emit("get-host", room);
+    const room = window.localStorage.getItem('roomId'); //the roomId is passed to the lobby through localStorage
+    socket.emit('get-host', room);
 
     loadUsers();
-    socket.on("render-users", (playerInfo) => {
+    socket.on('render-users', (playerInfo) => {
       setState({ ...state, players: playerInfo, host: playerInfo[0].nickname });
     });
 
-    socket.on("update-users", () => {
+    socket.on('update-users', () => {
       loadUsers();
     });
 
-    socket.on("begin-session", (puzzle) => {
-      const roomId = window.localStorage.getItem("roomId");
-      window.localStorage.setItem("puzzle", JSON.stringify(puzzle));
+    socket.on('begin-session', (puzzle) => {
+      const roomId = window.localStorage.getItem('roomId');
+      window.localStorage.setItem('puzzle', JSON.stringify(puzzle));
       props.history.push(`/game/${roomId}`);
       // props.history.push(`/${puzzle}/${room}`);
     });
   }, []);
 
   function loadUsers() {
-    const roomId = window.localStorage.getItem("roomId");
-    socket.emit("load-users", roomId);
+    const roomId = window.localStorage.getItem('roomId');
+    socket.emit('load-users', roomId);
   }
 
   //function to change which puzzle is currently selected:
   function selectPuzzle(puzzle) {
-    window.localStorage.setItem("puzzle", JSON.stringify(puzzle));
+    window.localStorage.setItem('puzzle', JSON.stringify(puzzle));
     setState({ ...state, selectedPuzzle: puzzle.date });
   }
 
   //handler function to push all players into a game room with the selected puzzle
   function startSession(puzzle) {
-    const roomId = window.localStorage.getItem("roomId");
+    const roomId = window.localStorage.getItem('roomId');
     selectPuzzle(puzzle);
-    socket.emit("start-session", roomId, puzzle);
+    socket.emit('start-session', roomId, puzzle);
     props.history.push(`/game/${roomId}`);
     // props.history.push(`/${state.selectedPuzzle}/${roomId}`);
   }
 
   //handler function to add the URL with room id to the clipboard
   function handleClick() {
-    const roomId = window.localStorage.getItem("roomId");
+    const roomId = window.localStorage.getItem('roomId');
     navigator.clipboard.writeText(`${window.location.host}/?` + roomId);
     // alert("Link copied to clipboard!");
   }
 
-  const isUserHost = window.localStorage.getItem("host");
-  const room = window.localStorage.getItem("roomId");
+  const isUserHost = window.localStorage.getItem('host');
+  const room = window.localStorage.getItem('roomId');
 
   return (
     <div className="splash-container">
@@ -106,9 +106,13 @@ export default function Lobby(props) {
           {crosswords.map((puzzle, ind) => {
             return (
               <div className="card" key={ind}>
+                <div className="puzzle-logo">
+                  <h1>{puzzle.difficulty[0].toUpperCase()}</h1>
+                  <p>{ind + 1}</p>
+                </div>
                 <div>{puzzle.difficulty}</div>
                 <div>{puzzle.name}</div>
-                {isUserHost === "true" ? (
+                {isUserHost === 'true' ? (
                   <Link to={`/game/${room}`}>
                     <button
                       type="submit"
