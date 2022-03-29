b6f5358d569a5935443982d2be5f97d70a850bbb
Lucas McGill <lmlucasmc8@gmail.com>
2022-03-29 14:49:48 -0400

    sound effect on last 5 seconds plus fanfare on end

--- client/components/Timer.js
+++ client/components/Timer.js
@@ -2,16 +2,35 @@ import React, { useEffect, useState } from 'react';
 import socket from './socket';
 
 export default function Timer() {
-  const [time, setTime] = useState(600);
+  const startTime = 10;
+  const [time, setTime] = useState(startTime);
   const room = window.localStorage.getItem('roomId');
   let secondsPassed = 0;
   useEffect(() => {
+    function sound(src) {
+      this.sound = document.createElement('audio');
+      this.sound.src = src;
+      this.sound.setAttribute('preload', 'auto');
+      this.sound.setAttribute('controls', 'none');
+      this.sound.style.display = 'none';
+      document.body.appendChild(this.sound);
+      this.play = function () {
+        this.sound.play();
+      };
+      this.stop = function () {
+        this.sound.pause();
+      };
+    }
+    const tick = new sound('/tick.mp3');
     const losecheck = setInterval(() => {
       secondsPassed++;
-      if (secondsPassed > 600) {
+      if (secondsPassed > startTime) {
         socket.emit('game-over', { roomId: room });
       }
-      setTime(600 - secondsPassed);
+      if (startTime - secondsPassed < 5) {
+        tick.play();
+      }
+      setTime(startTime - secondsPassed);
     }, 1000);
 
     return function cleanup() {
