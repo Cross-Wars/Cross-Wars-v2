a19a6155531e4a6a52bb9191b1b376a02291e1b0
abbasio <omarabbasi16@gmail.com>
2022-03-29 17:00:15 -0400

    Added 2014 crosswords

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -145,6 +145,7 @@ export default function Lobby(props) {
         <option value="2017">2017</option>
         <option value="2016">2016</option>
         <option value="2015">2015</option>
+        <option value="2014">2014</option>
       </select>
       <div className="card-container">
         {filterCrosswords.map((puzzle, ind) => {
