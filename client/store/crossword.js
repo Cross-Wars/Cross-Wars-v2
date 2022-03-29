77ed7fcfa9015f24a4f4bf582eb85dac35a4919f
abbasio <omarabbasi16@gmail.com>
2022-03-29 16:23:02 -0400

    Added filter for rendering crosswords by year

--- client/store/crossword.js
+++ client/store/crossword.js
@@ -1,8 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
-const GET_GUESS = "GET_GUESS";
-const GET_ALL_CROSSWORD = "GET_ALL_CROSSWORD";
+//---------ACTION CONSTANTS
+const GET_GUESS = 'GET_GUESS';
+const GET_ALL_CROSSWORD = 'GET_ALL_CROSSWORD';
+const GET_CROSSWORDS_BY_YEAR = 'GET_CROSSWORDS_BY_YEAR';
 
+//---------ACTION CREATORS
 export const getGuess = (row, col, char) => {
   return {
     type: GET_GUESS,
@@ -18,10 +21,18 @@ export const getAllCrossword = (crosswords) => {
   };
 };
 
+export const getCrosswordsByYear = (crosswords) => {
+  return {
+    type: GET_CROSSWORDS_BY_YEAR,
+    crosswords,
+  };
+};
+
+//---------THUNKS
 export const fetchAllCrossword = () => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.get("/api/crosswords");
+      const { data } = await axios.get('/api/crosswords');
       dispatch(getAllCrossword(data));
     } catch (err) {
       console.error(err);
@@ -29,8 +40,21 @@ export const fetchAllCrossword = () => {
   };
 };
 
+export const fetchCrosswordsByYear = (year) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/crosswords/${year}`);
+      dispatch(getCrosswordsByYear(data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
+
+//---------REDUCER
+
 let initialState = {
-  guess: "",
+  guess: '',
   allCrossword: [],
 };
 
@@ -38,8 +62,9 @@ export const dataReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_GUESS:
       return { ...state, guess: `${action.row} ${action.col} ${action.char}` };
-
     case GET_ALL_CROSSWORD:
+      return { ...state, allCrossword: action.crosswords };
+    case GET_CROSSWORDS_BY_YEAR:
       return { ...state, allCrossword: action.crosswords };
     default:
       return state;
