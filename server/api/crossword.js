6815e141b7b31237ee92b2c43f65b3cbb0f14181
abbasio <omarabbasi16@gmail.com>
2022-03-29 14:56:20 -0400

    Added dynamic express routes to select puzzles by year

--- server/api/crossword.js
+++ server/api/crossword.js
@@ -1,15 +1,36 @@
-const crosswordRouter = require("express").Router()
+const crosswordRouter = require('express').Router();
+const { Op } = require('sequelize');
 const {
   models: { Crossword },
-} = require("../db")
+} = require('../db');
 
-crosswordRouter.get("/", async (req, res, next) => {
+//GET /api/crosswords
+crosswordRouter.get('/', async (req, res, next) => {
   try {
-    const crosswords = await Crossword.findAll()
-    res.send(crosswords)
+    const crosswords = await Crossword.findAll();
+    res.send(crosswords);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-module.exports = crosswordRouter
+//GET /api/crosswords/:year
+crosswordRouter.get('/:year', async (req, res, next) => {
+  try {
+    const crosswords = await Crossword.findAll({
+      where: {
+        date: {
+          [Op.and]: {
+            [Op.gte]: new Date(req.params.year, 0, 1),
+            [Op.lte]: new Date(req.params.year, 11, 31),
+          },
+        },
+      },
+    });
+    res.send(crosswords);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = crosswordRouter;
