5a15cf1f4b51e809f9c71896ea92aa959038dc0f
sdokus <sami2464@gmail.com>
2022-03-31 18:03:51 -0400

    added responsive styling to the landing page

--- client/components/JoinOrCreateRoom.js
+++ client/components/JoinOrCreateRoom.js
@@ -1,24 +1,19 @@
-import React, { useState, useEffect } from "react"
-import socket from "./socket"
-import { uid } from "uid"
-import Logo from "./Logo"
-import Footer from "./Footer"
-import { Button } from "@material-ui/core"
-import { Link } from "react-router-dom"
-import generator from "./randomNamer"
-import { useSelector } from "react-redux"
-import Instructions from "./Instruction"
+import React, { useState, useEffect } from "react";
+import socket from "./socket";
+import { uid } from "uid";
+import Logo from "./Logo";
+import Footer from "./Footer";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import generator from "./randomNamer";
+import { useSelector } from "react-redux";
+import Instructions from "./Instruction";
 
 export default function JoinOrCreateRoom(props) {
-  const isLoggedIn = useSelector((state) => !!state.auth.id)
-  const userName = useSelector((state) => state.auth.username)
-  console.log(userName)
-  const name = isLoggedIn ? userName : "WordCrosser"
-  // const [newKey, setNewKey] = useState("");
-  // const [joinKey, setJoinKey] = useState("");
-  // const [roomId, setRoomId] = useState("");
-  // const [host, setHost] = useState(false);
-  // const [username, setUsername] = useState("user-name");
+  const isLoggedIn = useSelector((state) => !!state.auth.id);
+  const userName = useSelector((state) => state.auth.username);
+  const name = isLoggedIn ? userName : "WordCrosser";
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 700);
   const [state, setState] = useState({
     color: "blue",
     highlightBackground: "#9CC3D5FF",
@@ -26,34 +21,46 @@ export default function JoinOrCreateRoom(props) {
     host: false,
     roomId: "",
     socket: null,
-  })
+  });
+
+  //hook for re-rendering when the screen width changes:
   useEffect(() => {
-    setState({ ...state, nickname: name })
-  }, [isLoggedIn])
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
 
   useEffect(() => {
-    const puzzle = window.localStorage.getItem("puzzle")
-    console.log(puzzle)
+    setState({ ...state, nickname: name });
+  }, [isLoggedIn]);
+
+  useEffect(() => {
+    const puzzle = window.localStorage.getItem("puzzle");
+    console.log(puzzle);
     if (puzzle) {
-      window.localStorage.removeItem("puzzle")
-      document.location.reload()
+      window.localStorage.removeItem("puzzle");
+      document.location.reload();
     }
-    setState({ ...state, socket: socket })
-    const room = props.location.search.substring(1)
+    setState({ ...state, socket: socket });
+    const room = props.location.search.substring(1);
     if (!room) {
-      const newRoomId = uid()
-      setState({ ...state, roomId: newRoomId, host: true })
+      const newRoomId = uid();
+      setState({ ...state, roomId: newRoomId, host: true });
     } else {
-      setState({ ...state, roomId: room })
+      setState({ ...state, roomId: room });
     }
     socket.on("room-full", () => {
-      props.history.push("/error")
-    })
-  }, [])
+      props.history.push("/error");
+    });
+  }, []);
+
+  //helper function to add listener for changes in screen width
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 700);
+  };
 
   const handleNickNameChange = (evt) => {
-    setState({ ...state, nickname: evt.target.value })
-  }
+    setState({ ...state, nickname: evt.target.value });
+  };
 
   const handleColorChange = (evt) => {
     if (evt.target.value === "#D9514EFF") {
@@ -61,108 +68,149 @@ export default function JoinOrCreateRoom(props) {
         ...state,
         color: evt.target.value,
         highlightBackground: "#A9E5BBFF",
-      })
+      });
     } else if (evt.target.value === "orange") {
       setState({
         ...state,
         color: evt.target.value,
         highlightBackground: "#FBDE44FF",
-      })
+      });
     } else if (evt.target.value === "#0A5E2AFF") {
       setState({
         ...state,
         color: evt.target.value,
         highlightBackground: "#6DAC4FFF",
-      })
+      });
     } else if (evt.target.value === "#93385FFF") {
       setState({
         ...state,
         color: evt.target.value,
         highlightBackground: "#F99FC9FF",
-      })
+      });
     } else if (evt.target.value === "#D34F73FF") {
       setState({
         ...state,
         color: evt.target.value,
         highlightBackground: "#DBBEA1FF",
-      })
+      });
     } else if (evt.target.value === "#FF4F58FF") {
       setState({
         ...state,
         color: evt.target.value,
         highlightBackground: "#669DB3FF",
-      })
+      });
     } else if (evt.target.value === "#CE4A7EFF") {
       setState({
         ...state,
         color: evt.target.value,
         highlightBackground: "#DBBEA1FF",
-      })
+      });
     }
-  }
-
-  // const handleJoinKeyChange = (evt) => {
-  //   setJoinKey(evt.target.value);
-  // };
+  };
 
   const handleSubmit = (evt) => {
-    evt.preventDefault()
-    socket.emit("set-info", state)
-    socket.emit("join-room", state.roomId)
+    evt.preventDefault();
+    socket.emit("set-info", state);
+    socket.emit("join-room", state.roomId);
     window.localStorage.setItem(
       "color",
 
       `${state.color} ${state.highlightBackground}`
-    )
-    window.localStorage.setItem("focus", state.nickname)
-    window.localStorage.setItem("roomId", state.roomId)
-    window.localStorage.setItem("host", state.host)
-    props.history.push(`/lobby/${state.roomId}`)
-  }
+    );
+    window.localStorage.setItem("focus", state.nickname);
+    window.localStorage.setItem("roomId", state.roomId);
+    window.localStorage.setItem("host", state.host);
+    props.history.push(`/lobby/${state.roomId}`);
+  };
 
   return (
-    <div className="joinOrCreateRoom-container">
-      <div id="create-form">
-        <div id="landing-page-logo">
-          <Logo />
-        </div>
-        <form>
-          <label htmlFor="nickname-input">Enter Your Nickname</label>
-          <input
-            name="nickname-input"
-            onChange={handleNickNameChange}
-            type="text"
-            value={state.nickname}
-          />
-          <label htmlFor="color-select">Choose a Color</label>
-          <select defaultValue={state.color} onChange={handleColorChange}>
-            <option value="blue">Blue</option>
-            <option value="#D9514EFF">Red</option>
-            <option value="#0A5E2AFF">Dark Green</option>
-            <option value="#D34F73FF">Mystic</option>
-            <option value="#FF4F58FF">Fiery Coral</option>
-            <option value="orange">Orange</option>
-            <option value="#CE4A7EFF">Pink</option>
-            <option value="#93385FFF">Purple</option>
-          </select>
-        </form>
-        {props.location.search.substring(1) ? (
-          <form onSubmit={handleSubmit}>
-            <Button variant="contained" color="secondary" type="submit">
-              JOIN ROOM
-            </Button>
-          </form>
-        ) : (
-          <form onSubmit={handleSubmit}>
-            <Button variant="contained" color="secondary" type="submit">
-              CREATE ROOM
-            </Button>
-          </form>
-        )}
-        <Instructions />
-      </div>
+    <div>
+      {isDesktop ? (
+        <div className="joinOrCreateRoom-container">
+          <div id="create-form">
+            <div id="landing-page-logo">
+              <Logo />
+            </div>
+            <form>
+              <label htmlFor="nickname-input">Enter Your Nickname</label>
+              <input
+                name="nickname-input"
+                onChange={handleNickNameChange}
+                type="text"
+                value={state.nickname}
+              />
+              <label htmlFor="color-select">Choose a Color</label>
+              <select defaultValue={state.color} onChange={handleColorChange}>
+                <option value="blue">Blue</option>
+                <option value="#D9514EFF">Red</option>
+                <option value="#0A5E2AFF">Dark Green</option>
+                <option value="#D34F73FF">Mystic</option>
+                <option value="#FF4F58FF">Fiery Coral</option>
+                <option value="orange">Orange</option>
+                <option value="#CE4A7EFF">Pink</option>
+                <option value="#93385FFF">Purple</option>
+              </select>
+            </form>
+            {props.location.search.substring(1) ? (
+              <form onSubmit={handleSubmit}>
+                <Button variant="contained" color="secondary" type="submit">
+                  JOIN ROOM
+                </Button>
+              </form>
+            ) : (
+              <form onSubmit={handleSubmit}>
+                <Button variant="contained" color="secondary" type="submit">
+                  CREATE ROOM
+                </Button>
+              </form>
+            )}
+            <Instructions />
+          </div>
 
-      <Footer />
+          <Footer />
+        </div>
+      ) : (
+        <div className="joinOrCreateRoom-container">
+          <div id="create-form">
+            <form>
+              <label htmlFor="nickname-input">Enter Your Nickname</label>
+              <input
+                name="nickname-input"
+                onChange={handleNickNameChange}
+                type="text"
+                value={state.nickname}
+              />
+              <label htmlFor="color-select">Choose a Color</label>
+              <select defaultValue={state.color} onChange={handleColorChange}>
+                <option value="blue">Blue</option>
+                <option value="#D9514EFF">Red</option>
+                <option value="#0A5E2AFF">Dark Green</option>
+                <option value="#D34F73FF">Mystic</option>
+                <option value="#FF4F58FF">Fiery Coral</option>
+                <option value="orange">Orange</option>
+                <option value="#CE4A7EFF">Pink</option>
+                <option value="#93385FFF">Purple</option>
+              </select>
+            </form>
+            {props.location.search.substring(1) ? (
+              <form onSubmit={handleSubmit}>
+                <Button variant="contained" color="secondary" type="submit">
+                  JOIN ROOM
+                </Button>
+              </form>
+            ) : (
+              <form onSubmit={handleSubmit}>
+                <Button variant="contained" color="secondary" type="submit">
+                  CREATE ROOM
+                </Button>
+              </form>
+            )}
+            <Instructions />
+          </div>
+
+          <Footer />
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,80 +1,83 @@
-import React from "react"
-import { Link, useHistory } from "react-router-dom"
-import { connect, useDispatch, useSelector } from "react-redux"
-import Logo from "./Logo"
-import { Button } from "@material-ui/core"
-import { logout } from "../store"
-import Instructions from "./Instruction"
-import socket from "./socket"
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { connect, useDispatch, useSelector } from "react-redux";
+import Logo from "./Logo";
+import { Button } from "@material-ui/core";
+import { logout } from "../store";
+import Instructions from "./Instruction";
+import socket from "./socket";
 
 const Navbar = ({ handleClickLogout, isLoggedIn, username }) => {
-  const dispatch = useDispatch()
-  const history = useHistory()
+  const dispatch = useDispatch();
+  const history = useHistory();
   const handleClick = () => {
-    const room = window.localStorage.getItem("roomId")
+    const room = window.localStorage.getItem("roomId");
     if (room) {
-      socket.emit("leave-room", room)
+      socket.emit("leave-room", room);
     }
-    history.push("/")
-  }
+    history.push("/");
+  };
 
   return (
     <nav>
       {isLoggedIn ? (
         <div className="nav-bar">
-          <Button variant="contained" color="secondary" onClick={handleClick}>
-            Home
-          </Button>
-
           <div className="logo">
             <img src="/crosswars2.png" />
           </div>
+          <div className="nav-buttons">
+            <Button variant="contained" color="secondary" onClick={handleClick}>
+              Home
+            </Button>
 
-          <Button variant="contained" color="secondary">
-            <a href="#" onClick={handleClickLogout}>
-              Logout
-            </a>
-          </Button>
-          <Button variant="contained" color="secondary">
-            <Link to="/userProfile">UserProfile</Link>
-          </Button>
+            <Button variant="contained" color="secondary">
+              <a href="#" onClick={handleClickLogout}>
+                Logout
+              </a>
+            </Button>
+            <Button variant="contained" color="secondary">
+              <Link to="/userProfile">UserProfile</Link>
+            </Button>
+          </div>
         </div>
       ) : (
         <div className="nav-bar">
-          <Button variant="contained" color="secondary" onClick={handleClick}>
-            Home
-          </Button>
-
           <div className="logo">
             <img src="/crosswars2.png" />
           </div>
 
-          <Button variant="contained" color="secondary">
-            <Link to="/login">Login</Link>
-          </Button>
-          <Button variant="contained" color="secondary">
-            <Link to="/signup">Sign Up</Link>
-          </Button>
+          <div className="nav-buttons">
+            <Button variant="contained" color="secondary" onClick={handleClick}>
+              Home
+            </Button>
+
+            <Button variant="contained" color="secondary">
+              <Link to="/login">Login</Link>
+            </Button>
+            <Button variant="contained" color="secondary">
+              <Link to="/signup">Sign Up</Link>
+            </Button>
+          </div>
         </div>
       )}
     </nav>
-  )
-}
+  );
+};
 
 const mapState = (state) => {
   return {
     isLoggedIn: !!state.auth.id,
 
     username: state.auth.username,
-  }
-}
+  };
+};
 
 const mapDispatch = (dispatch) => {
   return {
     handleClickLogout() {
-      dispatch(logout())
+      dispatch(logout());
     },
-  }
-}
+  };
+};
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navbar);
